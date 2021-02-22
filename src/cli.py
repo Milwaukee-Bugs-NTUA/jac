1813a07c1d08a7b08775914d3c5b65874cf7d616
Giannis Fakinos <giannhs.afasias@gmail.com>
2021-02-22 23:06:03 +0200

    New cli architecture: cmd Library & click Framework

--- src/cli.py
+++ src/cli.py
@@ -2,55 +2,51 @@
 import click
 
 @click.group()
-def main_cli():
+def cli():
     pass
 
-@click.command()
+@cli.command()
 def join():
     click.echo("New node joined")
     raise NotImplementedError
 
-@click.command()
+@cli.command()
 @click.argument('key')
 def query(key):
     click.echo("Query for key {}".format(key))
     raise NotImplementedError
 
-@click.command()
+@cli.command()
 @click.argument('key')
 @click.argument('value')
 def insert(key, value):
     click.echo("Insert key {} with value {}".format(key,value))
     raise NotImplementedError
 
-@click.command()
+@cli.command()
 @click.argument('key')
 def delete(key):
     click.echo("Delete key {}".format(key))
     raise NotImplementedError
 
-@click.command()
+@cli.command()
 def depart():
     click.echo("Departure of node")
     raise NotImplementedError
 
-@click.command()
+@cli.command()
 def overlay():
     click.echo("Overlay")
     raise NotImplementedError
 
-@click.command()
+#   Dummy command, just for
+#   showing up in cli help message
+@cli.command()
 def help():
-    with click.Context(main_cli) as ctx:
-        click.echo(main_cli.get_help(ctx))
-
-main_cli.add_command(join)
-main_cli.add_command(query)
-main_cli.add_command(insert)
-main_cli.add_command(delete)
-main_cli.add_command(depart)
-main_cli.add_command(overlay)
-main_cli.add_command(help)
+    """
+        Prints this help message and exits.
+    """
+    pass
 
 if __name__ == "__main__":
-    main_cli()
+    cli()
--- src/jac.py
+++ src/jac.py
@@ -1,23 +1,60 @@
 #!/usr/bin/env python3
 import click
+import cmd
 from pyfiglet import Figlet
 import subprocess as sp
+
+from cli import *
+
+class JACShell(cmd.Cmd):
+
+    prompt = click.style("jac-cli@ntua",fg='cyan') + "$ "
+
+    def do_help(self, line):
+        command = cli.commands.get(line)
+        try:
+            if command:
+                with click.Context(command) as ctx:
+                    click.echo(command.get_help(ctx))
+            else:
+                with click.Context(cli) as ctx:
+                    click.echo(cli.get_help(ctx))
+        except:
+            cmd.Cmd.default(self, line)
+
+    def do_exit(self, inp):
+        click.echo("\n\nExiting jac shell...")
+        return True
+
+    def default(self, line):
+        args = line.split()
+        subcommand = cli.commands.get(args[0])
+        if subcommand:
+            try:
+                res = subcommand.main(args[1:],prog_name=args[0],standalone_mode = False)
+                if not res == 0:
+                    click.echo("Something went wrong")
+            except click.UsageError as usage_error:
+                usage_error.show()
+            except click.BadParameter as bad_parameter:
+                bad_parameter.show()
+            except click.Abort as abort_error:
+                abort_error.show()
+            except:
+                click.echo("Command ended unexpectedly")
+        else:
+            return cmd.Cmd.default(self, line)
+
+    # For using Ctrl-D as exit shortcut
+    do_EOF = do_exit
 
 def main():
     f = Figlet(font='slant')
     click.echo(f.renderText('J. A. C.'))
     click.echo("🎯 Just Another Chord implementation. 🎯\n")
-    while True:
-        try:
-            command = click.prompt(click.style("jac-cli@ntua",fg='cyan'),type=str,prompt_suffix=': ')
-            sp.run(['./cli.py'] + command.split(' '))
-            if command == "depart":
-                click.echo("\n\nExiting jac cli")
-                exit(0)
-        except click.Abort:
-            # Need to call depart command
-            click.echo("\n\nExiting jac cli")
-            exit(0)
+
+    jacshell = JACShell()
+    jacshell.cmdloop()
 
 if __name__ == "__main__":
     main()
