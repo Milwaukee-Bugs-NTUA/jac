133b479a64d4f880254629b7b8860f3f5e715f14
Giannis Fakinos <giannhs.afasias@gmail.com>
2021-03-10 15:28:20 +0200

    Use rsync

--- scripts/distribute_module.sh
+++ scripts/distribute_module.sh
@@ -2,5 +2,5 @@
 
 for i in 0 1 2 3
 do
-	rsync -av -e ssh --exclude='~/src/jacmodule/__pycache__' ~/src/ user@node$i:.
+	rsync -av -e ssh --exclude='~/src/jacmodule/__pycache__/*' ~/src/ user@node$i:.
 done
